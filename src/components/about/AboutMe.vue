3f2280ca72adc94aaa65110fc2ad6d055e2b832b
Marc Frankfurter <Marc.Frankfurter@scheppach.com>
2023-03-28 15:12:55 +0200

    updated about me section

--- src/components/about/AboutMe.vue
+++ src/components/about/AboutMe.vue
@@ -28,11 +28,11 @@ export default {
   <div class="block sm:flex sm:gap-10 mt-10 sm:mt-20">
     <!-- About profile image -->
     <div class="w-full sm:w-1/4 mb-7 sm:mb-0">
-      <img src="@/assets/images/profile.jpeg" class="rounded-xl w-96" alt=""/>
+      <img src="@/assets/images/me.png" class="rounded-xl w-96" alt=""/>
     </div>
 
     <!-- About details -->
-    <div class="w-full sm:w-3/4 text-left">
+    <div class="w-full sm:w-3/4 text-left align-text-bottom">
       <p v-for="bio in bios" :key="bio.id"
          class="font-general-regular mb-4 text-ternary-dark dark:text-ternary-light text-lg">
         {{ bio.bio }}
