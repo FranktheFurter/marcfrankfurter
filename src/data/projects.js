76557a860eaf70e69e0329ac5ad9c89836a79cda
Marc Frankfurter <Marc.Frankfurter@scheppach.com>
2023-03-28 13:39:16 +0200

    finally figured out this routing

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,42 +2,48 @@
 // Note: Here we are getting data from a js file, but in a different project I'll be fetching these projects from some srt of APIs.
 
 const projects = [
-	{
-		id: 1,
-		title: 'Custom ERP System with Blazor Server',
-		category: 'Web Application',
-		img: require('@/assets/images/erp-hero.png'),
-	},
-	{
-		id: 2,
-		title: 'Warehouse Management System Mobile & Flutter Web ',
-		category: 'Mobile Application',
-		img: require('@/assets/images/mobile-project-2.jpg'),
-	},
-	{
-		id: 3,
-		title: 'Returns / Repair Management System Flutter Web',
-		category: 'Web Application',
-		img: require('@/assets/images/ui-project-1.jpg'),
-	},
-	{
-		id: 4,
-		title: 'VW Asterix Integration',
-		category: 'UI/UX Design',
-		img: require('@/assets/images/ui-project-2.jpg'),
-	},
-	{
-		id: 5,
-		title: 'Porsche Asterix Integration',
-		category: 'UI/UX Design',
-		img: require('@/assets/images/mobile-project-1.jpg'),
-	},
-	{
-		id: 6,
-		title: 'Vinfast Amazon Alexa Integration',
-		category: 'UI/UX Design',
-		img: require('@/assets/images/web-project-1.jpg'),
-	},
+    {
+        id: 1,
+        title: 'Custom ERP System with Blazor Server',
+        category: 'Web Application',
+        img: require('@/assets/images/erp-hero.png'),
+        route: '/projects/blazor-erp',
+    },
+    {
+        id: 2,
+        title: 'Warehouse Management System Mobile & Flutter Web ',
+        category: 'Mobile Application',
+        img: require('@/assets/images/mobile-project-2.jpg'),
+        route: '/projects/test',
+    },
+    {
+        id: 3,
+        title: 'Returns / Repair Management System Flutter Web',
+        category: 'Web Application',
+        img: require('@/assets/images/ui-project-1.jpg'),
+        route: '/projects/test',
+    },
+    {
+        id: 4,
+        title: 'VW Asterix Integration',
+        category: 'UI/UX Design',
+        img: require('@/assets/images/ui-project-2.jpg'),
+        route: '/projects/test',
+    },
+    {
+        id: 5,
+        title: 'Porsche Asterix Integration',
+        category: 'UI/UX Design',
+        img: require('@/assets/images/mobile-project-1.jpg'),
+        route: '/projects/test',
+    },
+    {
+        id: 6,
+        title: 'Vinfast Amazon Alexa Integration',
+        category: 'UI/UX Design',
+        img: require('@/assets/images/web-project-1.jpg'),
+        route: '/projects/test',
+    },
 ];
 
 export default projects;
