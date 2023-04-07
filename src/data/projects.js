cc6dbe419494dbfea6821915986264b3f627ae92
Marc Frankfurter <Marc.Frankfurter@scheppach.com>
2023-04-07 16:57:03 +0200

    cleanup

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,6 +1,3 @@
-// Projects data.
-// Note: Here we are getting data from a js file, but in a different project I'll be fetching these projects from some srt of APIs.
-
 const projects = [
   {
     id: 1,
